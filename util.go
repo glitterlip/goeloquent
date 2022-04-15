a3ea4d5882641785634ebdd583060adc15ecd091
glitter <worldtongfb@gmail.com>
2022-04-15 23:24:35 +0800

    fix(builder): struct

--- util.go
+++ util.go
@@ -18,13 +18,19 @@ func ExtractStruct(target interface{}) map[string]interface{} {
 	tv := reflect.Indirect(reflect.ValueOf(target))
 	tt := tv.Type()
 	result := make(map[string]interface{}, tv.NumField())
-	for i := 0; i < tv.NumField(); i++ {
-		//when using simple struct ,provide a convenient way to set table
-		if tt.Field(i).Name == "Table" && strings.Contains(tt.Field(i).Tag.Get("goelo"), "TableName:") {
-			continue
+	if tt.Kind() == reflect.Struct {
+		m := GetParsedModel(tt)
+		for _, key := range m.DbFields {
+			keyIndex := m.FieldsByDbName[key].Index
+			if !tv.Field(keyIndex).IsZero() {
+				result[key] = tv.Field(keyIndex).Interface()
+			}
 		}
-		key := ToSnakeCase(tt.Field(i).Name)
-		result[key] = tv.Field(i).Interface()
+	} else {
+		for i := 0; i < tv.NumField(); i++ {
+			key := ToSnakeCase(tt.Field(i).Name)
+			result[key] = tv.Field(i).Interface()
+		}
 	}
 	return result
 }
