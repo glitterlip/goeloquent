f985f08e87cd087828ff894d73b9ad300c0df3e0
glitter <worldtongfb@gmail.com>
2022-05-25 19:32:10 +0800

    fix(model): parse

--- model.go
+++ model.go
@@ -272,11 +272,12 @@ func (m *Model) ParseField(field reflect.StructField) *Field {
 				//todo case defaultvalue,case updateable,insertable
 			}
 		}
-	} else {
+		//} else {
+		//TODO: need more test
 		//maybe only a few fileds has goelo tag
-		modelField.ColumnName = ToSnakeCase(field.Name)
-		m.FieldsByDbName[modelField.ColumnName] = modelField
-		m.DbFields = append(m.DbFields, modelField.ColumnName)
+		//modelField.ColumnName = ToSnakeCase(field.Name)
+		//m.FieldsByDbName[modelField.ColumnName] = modelField
+		//m.DbFields = append(m.DbFields, modelField.ColumnName)
 	}
 	//if  { ignored tag
 	m.Fields = append(m.Fields, modelField)
