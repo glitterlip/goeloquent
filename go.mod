dae5c9d19b088a52e828a08ef2a99278d559bb7e
glitter <worldtongfb@gmail.com>
2022-07-12 14:17:51 +0800

    feat(builder): pagination last pages

--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.16
 require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/tools v0.1.10 // indirect
 )
--- paginator.go
+++ paginator.go
@@ -1,8 +1,14 @@
 package goeloquent
 
+import "math"
+
 type Paginator struct {
 	Items       interface{}
 	Total       int64
 	PerPage     int64
 	CurrentPage int64
+}
+
+func (p Paginator) LastPage() int64 {
+	return int64(int(math.Ceil(float64(p.Total / p.PerPage))))
 }
