b08320bfa4ffcc2c9f6e7d012bf68e7bdbfd0ab5
glitter <worldtongfb@gmail.com>
2023-03-16 09:16:08 +0800

    feat(pagination): json format

--- paginator.go
+++ paginator.go
@@ -3,10 +3,10 @@ package goeloquent
 import "math"
 
 type Paginator struct {
-	Items       interface{}
-	Total       int64
-	PerPage     int64
-	CurrentPage int64
+	Items       interface{} `json:"items"`
+	Total       int64       `json:"total"`
+	PerPage     int64       `json:"per_page"`
+	CurrentPage int64       `json:"current_page"`
 }
 
 func (p Paginator) LastPage() int64 {
