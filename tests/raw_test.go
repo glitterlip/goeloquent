6339b67e6b821ab6f798db1290aee5e131ee3437
glitter <worldtongfb@gmail.com>
2024-07-08 23:16:54 +0800

    test(package): raw test

--- tests/raw_test.go
+++ tests/raw_test.go
@@ -1,7 +1,6 @@
 package tests
 
 import (
-	"github.com/glitterlip/goeloquent"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -22,56 +21,11 @@ func TestRawMethods(t *testing.T) {
 		count, _ = insert.RowsAffected()
 		assert.Equal(t, int64(2), count)
 
-		//test select map
-		var row = make(map[string]interface{})
-		r, err := DB.Select("select * from users where name = ? ", []interface{}{"Alice"}, &row)
-		assert.Nil(t, err)
-		assert.Equal(t, "Alice", string(row["name"].([]uint8)))
+		var countUsers int64
 
-		//test select slice of map
-		var rows []map[string]interface{}
-		r, err = DB.Select("select * from users ", nil, &rows)
+		_, err = DB.Select("select count(*) from users", []interface{}{}, &countUsers)
 		assert.Nil(t, err)
-		assert.Equal(t, int64(3), r.Count)
-		goeloquent.ParsedModelsMap.Delete("github.com/glitterlip/goeloquent/tests.User")
-		//test select struct
-		type User struct {
-			Id   int64  `goelo:"column:id"`
-			Name string `goelo:"column:name"`
-			Age  int    `goelo:"column:age"`
-		}
-		type MappingUser struct {
-			Id       int64  `goelo:"column:id"`
-			UserName string `goelo:"column:name"`
-			Age      int    `goelo:"column:age"`
-		}
-		var user User
-		var mapUser MappingUser
-		res, err := DB.Select("select * from users order by id asc limit ?", []interface{}{1}, &user)
-		assert.Nil(t, err)
-		assert.Equal(t, int64(1), res.Count)
-		assert.Equal(t, int64(1), user.Id)
-		assert.Equal(t, 33, user.Age)
-		assert.Equal(t, "Alice", user.Name)
-
-		res, err = DB.Select("select * from users order by id asc limit ? offset 1", []interface{}{1}, &mapUser)
-		assert.Nil(t, err)
-		assert.Equal(t, int64(1), res.Count)
-		assert.Equal(t, int64(2), mapUser.Id)
-		assert.Equal(t, 12, mapUser.Age)
-		assert.Equal(t, "John", mapUser.UserName)
-
-		//select slice of struct
-		var us []User
-		res, err = DB.Select("select * from users order by id asc limit ?", []interface{}{2}, &us)
-		assert.Nil(t, err)
-		assert.Equal(t, int64(2), res.Count)
-		assert.Equal(t, int64(1), us[0].Id)
-		assert.Equal(t, int64(2), us[1].Id)
-		assert.Equal(t, 33, us[0].Age)
-		assert.Equal(t, 12, us[1].Age)
-		assert.Equal(t, "Alice", us[0].Name)
-		assert.Equal(t, "John", us[1].Name)
+		assert.Equal(t, int64(3), countUsers)
 
 		//test update
 		u, err := DB.Update("update users set age = 18 where age < 18 ", []interface{}{})
--- tests/test_helper.go
+++ tests/test_helper.go
@@ -88,3 +88,104 @@ func RunWithDB(create, drop interface{}, test func()) {
 	test()
 
 }
+func CreateRelationTables() (create, drop string) {
+	create = `
+DROP TABLE IF EXISTS "comment","image","post","tag","tagables","user","users","view_record","friends","address";
+CREATE TABLE "comment" (
+  "cid" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "post_id" int(11) NOT NULL,
+  "content" varchar(255) DEFAULT NULL,
+  "user_id" int(11) NOT NULL,
+  "upvotes" int(11) NOT NULL DEFAULT '0',
+  "downvotes" int(11) NOT NULL DEFAULT '0',
+  "comment_id" int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY ("cid") USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE "image" (
+  "id" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "url" varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  "imageable_id" int(11) DEFAULT NULL,
+  "imageable_type" varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY ("id")
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE "post" (
+  "pid" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "title" varchar(255) NOT NULL DEFAULT '',
+  "author_id" int(10) unsigned NOT NULL DEFAULT '0',
+  "created_at" datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" datetime DEFAULT NULL,
+  "deleted_at" datetime DEFAULT NULL,
+  PRIMARY KEY ("pid") USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE "tag" (
+  "tid" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "name" varchar(255) DEFAULT '',
+  PRIMARY KEY ("tid") USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE "tagables" (
+  "id" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "tag_id" int(11) NOT NULL,
+  "tagable_id" int(11) DEFAULT NULL,
+  "status" int(10) unsigned NOT NULL DEFAULT '0',
+  "tagable_type" varchar(255) DEFAULT NULL,
+  PRIMARY KEY ("id")
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE "user" (
+  "id" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "age" int(10) unsigned NOT NULL DEFAULT '0',
+  "name" varchar(255) NOT NULL DEFAULT '',
+  "created_at" datetime DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" datetime DEFAULT NULL,
+  PRIMARY KEY ("id") USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE "view_record" (
+  "id" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "user_id" int(11) DEFAULT NULL,
+  "post_id" int(11) DEFAULT NULL,
+  "view_time" datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY ("id")
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE "friends" (
+  "id" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "user_id" int(11) NOT NULL,
+  "friend_id" int(11) NOT NULL,
+  "status" tinyint(4) NOT NULL,
+  "time" datetime DEFAULT NULL,
+  "additional" varchar(255) DEFAULT '',
+  PRIMARY KEY ("id")
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE "address" (
+  "id" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "user_id" int(10) unsigned NOT NULL,
+  "country" tinyint(4) NOT NULL,
+  "state" varchar(255) NOT NULL,
+  "city" varchar(255) NOT NULL,
+  "detail" varchar(255) DEFAULT NULL,
+  PRIMARY KEY ("id")
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+`
+	drop = `
+DROP TABLE IF EXISTS "comment","image","post","tag","tagables","user","users","view_record","friends","address";
+`
+	return
+}
+
+func UserTableSql() (create, drop string) {
+	create = `
+DROP TABLE IF EXISTS users;
+CREATE TABLE "users" (
+  "id" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "name" varchar(255) NOT NULL,
+  "info" json NULL,
+  "roles" varchar(255),
+  "age" tinyint(10) unsigned NOT NULL DEFAULT '0',
+  "status" tinyint(10) unsigned NOT NULL DEFAULT '0',
+  "created_at" datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  "deleted_at" datetime DEFAULT NULL,
+  PRIMARY KEY ("id")
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
+`
+	drop = `DROP TABLE IF EXISTS users`
+	return
+}
