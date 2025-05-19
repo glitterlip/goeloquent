328443d4258a1df68efe483f3a5d99f63952e044
glitter <worldtongfb@gmail.com>
2025-05-19 13:44:46 +0800

    feat(event): events

---
+++ events.go
@@ -0,0 +1,10 @@
+package goeloquent
+
+const (
+	EventConnectionOpened     = "connection:opened"
+	EventQueryExecuted        = "query:executed"
+	EventTransactionBegin     = "transaction:begin"
+	EventTransactionCommitted = "transaction:committed"
+	EventTransactionRollback  = "transaction:rollback"
+	EventError                = "error"
+)
