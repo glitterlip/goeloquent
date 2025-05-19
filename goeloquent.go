48a7bd807bcdef79a1cdf98f3705669664bf72b7
glitter <worldtongfb@gmail.com>
2025-05-19 15:05:38 +0800

    feat(package): init

---
+++ goeloquent.go
@@ -0,0 +1,35 @@
+package goeloquent
+
+import (
+	"database/sql"
+	"sync"
+)
+
+var DB *DatabaseManager
+
+func init() {
+
+	DB = &DatabaseManager{
+		Connections: sync.Map{},
+	}
+}
+
+func Open(name string, config DBConfig) (Connection, error) {
+	if conn, ok := DB.Connections.Load(name); ok {
+		return conn.(*MysqlConnection), nil
+	}
+	db, err := sql.Open(string(config.Driver), config.DSN)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	connection := MysqlConnection{
+		DBConfig: &config,
+		DB:       db,
+	}
+
+	DB.Connections.Store(name, &connection)
+	return &connection, nil
+}
