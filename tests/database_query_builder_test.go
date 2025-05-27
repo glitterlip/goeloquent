1e418823402303c3ecd56e3d4e47bbb56b98aa54
glitter <worldtongfb@gmail.com>
2025-05-27 19:40:04 +0800

    test(grammar): alias

--- tests/database_query_builder_test.go
+++ tests/database_query_builder_test.go
@@ -70,3 +70,55 @@ func TestAliasWrappingAsWholeConstant(t *testing.T) {
 	assert.Nil(t, query.Statement.Error)
 	assert.Equal(t, query.ToSql(), "select `x`.`y` as `foo.bar` from `baz`")
 }
+
+func TestAliasWrappingWithSpacesInDatabaseName(t *testing.T) {
+	b1 := GetBuilder()
+	b1.Select("w x.y.z as foo.bar").From("baz")
+	assert.Equal(t, "select `w x`.`y`.`z` as `foo.bar` from `baz`", b1.ToSql())
+}
+
+func TestAddingSelects(t *testing.T) {
+	b := GetBuilder()
+	b.Select("foo").AddSelect("bar").AddSelect("baz", "boom").AddSelect("bar").From("users")
+	assert.Nil(t, b.Statement.Error)
+	assert.Equal(t, "select `foo`, `bar`, `baz`, `boom`, `bar` from `users`", b.ToSql())
+}
+
+func TestBasicSelectWithPrefix(t *testing.T) {
+	b := GetBuilder()
+	b.Grammar.SetTablePrefix("prefix_")
+	b.From("users").Select()
+	assert.Equal(t, "select * from `prefix_users`", b.ToSql())
+}
+
+func TestBasicSelectDistinct(t *testing.T) {
+	b := GetBuilder()
+	b.Distinct().Select("foo", "bar").From("users")
+	assert.Equal(t, "select distinct `foo`, `bar` from `users`", b.ToSql())
+}
+func TestBasicSelectDistinctOnColumns(t *testing.T) {
+	b := GetBuilder()
+	b.Distinct("foo").Select("foo", "bar").From("users")
+	assert.Equal(t, "select distinct `foo`, `bar` from `users`", b.ToSql())
+}
+func TestBasicAlias(t *testing.T) {
+	b := GetBuilder()
+	b.Select("foo as bar").From("users")
+	assert.Equal(t, "select `foo` as `bar` from `users`", b.ToSql())
+
+}
+func TestAliasWithPrefix(t *testing.T) {
+
+	b1 := GetBuilder()
+	b1.Grammar.SetTablePrefix("prefix_")
+	b1.Select("foo as bar", "baz").From("users as people")
+	assert.Equal(t, "select `foo` as `bar`, `baz` from `prefix_users` as `prefix_people`", b1.ToSql())
+}
+
+func TestJoinAliasesWithPrefix(t *testing.T) {
+	b := GetBuilder()
+	b.Grammar.SetTablePrefix("prefix_")
+	b.Select("*").From("services").Join("translations AS t", "t.item_id", "=", "services.id")
+	assert.Equal(t, "select * from `prefix_services` inner join `prefix_translations` as `prefix_t` on `prefix_t`.`item_id` = `prefix_services`.`id`", b.ToSql())
+
+}
