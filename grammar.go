d5945f0ad41cc500ec1ac759734c74da87e7ef82
glitter <worldtongfb@gmail.com>
2025-07-27 17:30:58 +0800

    fix(grammar): upsert

--- grammar.go
+++ grammar.go
@@ -325,12 +325,12 @@ func (g *MysqlGrammar) CompileInsert(query *QueryBuilder, values []map[string]in
 
 	return fmt.Sprintf("insert into %s (%s) values %s", g.WrapTable(query.FromTable), columns, strings.Join(sqls, ", ")), res
 }
-func (g *MysqlGrammar) CompileUpsert(query *QueryBuilder, values []map[string]interface{}, uniqueBy []string, update map[string]interface{}) (string, []interface{}) {
+func (g *MysqlGrammar) CompileUpsert(query *QueryBuilder, values []map[string]interface{}, uniqueBy []string, update []string) (string, []interface{}) {
 	sql, bindings := g.CompileInsert(query, values)
 	sql += " on duplicate key update "
 	var parts []string
-	for key, value := range update {
-		parts = append(parts, g.Wrap(key)+" = "+g.Parameter(value))
+	for _, value := range update {
+		parts = append(parts, g.Wrap(value)+" = values("+g.Wrap(value)+")")
 	}
 	return sql + strings.Join(parts, ", "), bindings
 
