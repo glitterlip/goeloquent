b27b439cd628581fcc434ed0efec5901f1995db2
glitter <worldtongfb@gmail.com>
2025-08-11 17:39:07 +0800

    test(model): fillable

---
+++ tests/model_attribute_test.go
@@ -0,0 +1,40 @@
+package tests
+
+import (
+	"github.com/glitterlip/goeloquent/v2"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type FillableModel struct {
+	*goeloquent.EloquentModel
+	Id    int64  `json:"id" goelo:"column:id;primaryKey;"`
+	Name  string `json:"name" goelo:"column:name;"`
+	Email string `json:"email" goelo:"column:email;"`
+}
+
+func (f *FillableModel) GetFillable() map[string]struct{} {
+	return map[string]struct{}{
+		"name": {},
+	}
+}
+func TestFillables(t *testing.T) {
+
+	model := &FillableModel{}
+	parsed, err := goeloquent.ParseModel(model)
+	assert.Nil(t, err)
+
+	assert.Equal(t, map[string]struct{}{
+		"Name": {},
+		"name": {},
+	}, parsed.Fillables)
+
+	model.Init(model).Fill(map[string]interface{}{
+		"name":  "test",
+		"email": "john@github.com",
+		"Email": "john@github.com",
+	})
+
+	assert.Equal(t, "test", model.Name)
+	assert.Equal(t, "", model.Email)
+}
