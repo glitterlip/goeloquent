47df9db7d25eaeeb66287f425d8d5e7152152c08
glitter <worldtongfb@gmail.com>
2025-08-20 00:00:29 +0800

    feat(event): events

--- events.go
+++ events.go
@@ -1,10 +1,36 @@
 package goeloquent
 
+type EventName string
+
 const (
-	EventConnectionOpened     = "connection:opened"
-	EventQueryExecuted        = "query:executed"
-	EventTransactionBegin     = "transaction:begin"
-	EventTransactionCommitted = "transaction:committed"
-	EventTransactionRollback  = "transaction:rollback"
-	EventError                = "error"
+	EventConnectionOpened      EventName = "connection:opened"
+	EventQueryExecuted         EventName = "query:executed"
+	EventTransactionBegin      EventName = "transaction:begin"
+	EventTransactionCommitting EventName = "transaction:committing"
+	EventTransactionCommitted  EventName = "transaction:committed"
+	EventTransactionRolledback EventName = "transaction:rolledback"
+	EventError                 EventName = "error"
+)
+
+type ModelEventName string
+
+const (
+	EventModelBooting       ModelEventName = "model:booting"
+	EventModelBoot          ModelEventName = "model:boot"
+	EventModelBooted        ModelEventName = "model:booted"
+	EventModelCreating      ModelEventName = "model:creating"
+	EventModelCreated       ModelEventName = "model:created"
+	EventModelSaving        ModelEventName = "model:saving"
+	EventModelSaved         ModelEventName = "model:saved"
+	EventModelUpdating      ModelEventName = "model:updating"
+	EventModelUpdated       ModelEventName = "model:updated"
+	EventModelDeleting      ModelEventName = "model:deleting"
+	EventModelDeleted       ModelEventName = "model:deleted"
+	EventModelTrashed       ModelEventName = "model:trashed"
+	EventModelForceDeleting ModelEventName = "model:force_deleting"
+	EventModelForceDeleted  ModelEventName = "model:force_deleted"
+	EventModelRestoring     ModelEventName = "model:restoring"
+	EventModelRestored      ModelEventName = "model:restored"
+	EventModelRetrieved     ModelEventName = "model:retrieved"
+	EventModelReplicating   ModelEventName = "model:replicating"
 )
