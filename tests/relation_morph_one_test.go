1eb3338792b2c45a3d001a4fef455fd23b710da7
glitter <worldtongfb@gmail.com>
2025-08-31 17:00:15 +0800

    test(relation): morph one quote table

--- tests/relation_morph_one_test.go
+++ tests/relation_morph_one_test.go
@@ -111,7 +111,7 @@ func TestModelsAreProperlyMatchedToParentsMorphOne(t *testing.T) {
 		_, err = conn.Model(&MorphOneUser{}).With("Image").Get(&users)
 		assert.Nil(t, err)
 		assert.Equal(t, len(sts), 2)
-		assert.Equal(t, sts[1].RawSql, "select * from `images` where `imageable_type` = ? and `imageable_id` is not null and `imageable_id` in (?, ?, ?, ?, ?)")
+		assert.Equal(t, sts[1].RawSql, "select * from `images` where select * from `images` where `images`.`imageable_type` = ? and `images`.`imageable_id` is not null and `images`.`imageable_id` in (?, ?, ?, ?, ?)")
 		assert.Equal(t, []interface{}{"MorphOneUser", int64(1), int64(2), int64(3), int64(4), int64(5)}, sts[1].GetBindings())
 		assert.Equal(t, 5, len(users))
 		assert.Equal(t, users[0].Image.ImageableId, users[0].Id)
